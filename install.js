d5eeb9658005eb0739c6df9fdbbd50c88161cf50
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-04-29 16:59:09 +0200

    feat: add pretty bar in install.js

--- install.js
+++ install.js
@@ -7,30 +7,51 @@
 - And voila !
 */
 
-const fetch = require("node-fetch");
-var fs = require('fs');
-var request = require('request');
-var progress = require('request-progress');
+const fetch = require("node-fetch")
+const fs = require('fs')
+const request = require('request')
+const progress = require('request-progress')
+const progressBar = require('cli-progress')
+const prettyBytes = require('pretty-bytes')
+const chalk = require('chalk')
 
+const fileName = "karate.jar"
 
-fetch("https://api.github.com/repos/intuit/karate/releases", {
-  "method": "GET"
-})
-.then(async (response) => {
-    let json = await response.json()
-    let url = json[0].assets.filter(r => r.name.indexOf('.jar') != -1)[0].browser_download_url
-    console.log(url)
-    progress(request(url), {
-    })
-    .on('progress', function (state) {
-        console.log('progress', state);
-    })
-    .on('error', function (err) {
-      console.error(err);
-    })
-    .on('end', function () {
-      console.log("FIN CONNARD");
-    })
-    .pipe(fs.createWriteStream("karate.jar"));
+const bar = new progressBar.SingleBar({
+  format: '> ' + fileName + ' |' + chalk.green('{bar}') + '| {percentage}% '
+    + chalk.gray('||') + ' {value}/{total} Bytes ' + chalk.gray('||')
+    + 'Speed: ' + chalk.cyan('{speed}/sec'),
+  barCompleteChar: '\u2588',
+  barIncompleteChar: '\u2591',
+  hideCursor: true
+});
+let total = null
+
+fetch("https://api.github.com/repos/intuit/karate/releases").then(async (response) => {
+  let json = await response.json()
+  let url = json[0].assets.filter(r => r.name.indexOf('.jar') != -1)[0].browser_download_url
+  console.log("> Downloading " + url)
+  console.log()
+  progress(request(url))
+  .on('progress', function (state) {
+      if (total === null) {
+        total = state.size.total
+        bar.start(total, 0, {speed: "N/A"})
+      }
+      bar.update(state.size.transferred, {
+        speed: state.speed != null ? prettyBytes(state.speed) : "N/A"
+      })
+  })
+  .on('error', function (err) {
+    console.error(err);
+  })
+  .on('end', function () {
+    bar.update(total);
+    console.log()
+    console.log()
+    console.log("> Downloaded " + fileName);
+    bar.stop()
+  })
+  .pipe(fs.createWriteStream(fileName));
  })
 
